b52f5a99f187449c5af7f30303369b08b93beac6
aaronBuff <70406865+aaronBuff@users.noreply.github.com>
2022-04-12 19:07:23 -0600

    SQLAlcehemy to initialize database and query dataframes

---
+++ database/data.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from flask_mysqldb import MySQL
+from flask import current_app, render_template,Flask
+from flaskSQL import BTC,ETH,XMR
+from collections import defaultdict
+from sqlalchemy.inspection import inspect
+
+app = Flask(__name__)
+
+def query_to_dict(rset):
+    result = defaultdict(list)
+    for obj in rset:
+        instance = inspect(obj)
+        for key, x in instance.attrs.items():
+            result[key].append(x.value)
+    return result
+
+def get_crypto():
+    btc=pd.DataFrame(query_to_dict(BTC.query.all()))
+    eth=pd.DataFrame(query_to_dict(ETH.query.all()))
+    xmr=pd.DataFrame(query_to_dict(XMR.query.all()))
+    data=[btc,eth,xmr]
+    # print(data)
+    return data
+
