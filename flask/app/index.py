7691503d731ccafa213ca4d9a36745178d1072af
aaronBuff <70406865+aaronBuff@users.noreply.github.com>
2022-04-13 16:57:18 -0600

    better working version

--- flask/app/index.py
+++
@@ -1,9 +0,0 @@
-from flask import current_app, render_template
-from .data import get_crypto
-app = current_app
-
-@app.route("/")
-def home():
-    data=get_crypto()
-    print(data[0],data[1])
-    return render_template("index.html",plot_urlbtc=data[0],plot_urleth=data[1])
--- flask/app/test.py
+++
@@ -1,3 +0,0 @@
-from data import get_crypto
-
-get_crypto()
